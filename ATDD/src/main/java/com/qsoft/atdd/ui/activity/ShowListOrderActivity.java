a26b2c69e8e933821fea1247ca0f5c7e9f78ec81
thanhtd <thanhtd@qsoftvietnam.com>
2014-03-28 14:44:58 +0700

    action listview and logout

--- ATDD/src/main/java/com/qsoft/atdd/ui/activity/ShowListOrderActivity.java
+++ ATDD/src/main/java/com/qsoft/atdd/ui/activity/ShowListOrderActivity.java
@@ -1,9 +1,12 @@
 package com.qsoft.atdd.ui.activity;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.widget.ListView;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
 import com.qsoft.atdd.R;
 
 /**
@@ -12,9 +15,11 @@ import com.qsoft.atdd.R;
 @EActivity(R.layout.list_goods)
 public class ShowListOrderActivity extends FragmentActivity
 {
+    @ViewById(R.id.list_goods_lvListOrder)
+    ListView listOrder;
+
 
     @Click(R.id.list_goods_btAdd)
-
     void addOrder()
     {
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -22,6 +27,14 @@ public class ShowListOrderActivity extends FragmentActivity
         addOrderFragment.show(fragmentManager,"abc");
     }
 
+
+
+    @Click(R.id.list_order_ivLogOut)
+    void logout()
+    {
+        Intent intent = new Intent(this,MainActivity_.class);
+        startActivity(intent);
+    }
 
 
 }
